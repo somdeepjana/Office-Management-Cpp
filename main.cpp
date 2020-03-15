c5babe6412b278cc32d8be9bba8739ddefe382e8
Somdeep Jana <somdeepjana@outlook.in>
2020-03-15 11:10:16 +0530

    Project Creation
    Initializing the Project Enviorment

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//Required Namespaces Only
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main(){
+	
+	cout<<"Office Management System"<<endl;
+	
+	return 0;
+}
